6f196701fedf72be92cd400ffdaa5891952307eb
whatasoda <git@whatasoda.me>
2019-10-07 13:10:35 +0900

    test: update

--- tests/__tests__/counter.mock.test.tsx
+++ tests/__tests__/counter.mock.test.tsx
@@ -36,15 +36,23 @@ describe('Counter with mock component', () => {
     story.snapshot();
 
     story.setProps({ setDozen });
-    story.snapshot('"setDozen" should be called once');
+    story.snapshot(`
+      'setDozen' should be called once
+    `);
 
     Array.from({ length: 11 }).forEach(() => {
       story.dispatch('0: increment');
     });
-    story.snapshot('"setDozen" must not be called');
+    story.snapshot(`
+      'setDozen must not be called
+      'length' of Conunter should be 11
+      render call should be 12
+    `);
 
     story.dispatch('0: increment');
-    story.snapshot('"setDozen" should be called once more');
+    story.snapshot(`
+      'setDozen' should be called once more
+    `);
 
     story.finish();
   });
