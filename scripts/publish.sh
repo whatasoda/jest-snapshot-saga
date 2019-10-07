c80b1844e5b1ad1c60d70b93f12cf1d5502f96c0
whatasoda <git@whatasoda.me>
2019-10-07 11:55:49 +0900

    fix: add --no-abbrev

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -ex
 
-TAG_VERSION=`git describe 2>/dev/null`
+TAG_VERSION=`git describe --no-abbrev 2>/dev/null`
 REPO_VERSION=`npm view jest-snapshot-saga version 2>/dev/null`
 LOCAL_VERSION=`node -p "require('./package.json').version"`
 
 if [[ "$REPO_VERSION" = "$LOCAL_VERSION" ]]; then
   echo "This version is already exists"
 elif [[ "$TAG_VERSION" != "v$LOCAL_VERSION" ]]; then
-  echo "Tag is unmatched"
+  echo "Tag unmatched"
 else 
   npx can-npm-publish
   npm publish
